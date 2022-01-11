39ca955fd82b62c9c17721b6c35ad736d616f869
SeHoon <sehoon1989@gmail.com>
2022-01-11 23:30:27 +0900

    add-webpack-bundle-analyzer

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,12 @@
 module.exports = {
   transpileDependencies: ['vuetify'],
+  pluginOptions: {
+    webpackBundleAnalyzer: {
+      openAnalyzer: false, // 자동으로 open하지 않음
+      analyzerMode: 'static' /* 분석파일 html 보고서를 dist 폴더에 저장한다  */,
+      reportFilename: 'manbalboy-bundle-report.html', // 리포트이름
+    },
+  },
   devServer: {
     proxy: {
       '/api': {
